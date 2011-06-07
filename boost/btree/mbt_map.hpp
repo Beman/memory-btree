fe30f92e8a33b8a178a045365a6ed1d9c9c249d2
Beman <bdawes@acm.org>
2011-06-07 14:12:25 -0400

    Add (yet again) derived types leaf_node and branch_node. Factor out insert() code common to leaves and branches into m_insert(). Tests passing, although node splits not test yet.

--- boost/btree/mbt_map.hpp
+++ boost/btree/mbt_map.hpp
@@ -27,6 +27,8 @@ namespace btree {
   class mbt_map   // short for memory_btree_map
   {
     class node;
+    class leaf_node;
+    class branch_node;
     template <class VT>
       class iterator_type;
   public:
@@ -116,8 +118,8 @@ namespace btree {
     const_reverse_iterator  crend() const noexcept;
 
     // capacity:
-    bool                    empty() const noexcept {return m_size == 0;}
-    size_type               size() const noexcept  {return m_size;}
+    bool                    empty() const noexcept     {return m_size == 0;}
+    size_type               size() const noexcept      {return m_size;}
     size_type               max_size() const noexcept;
 
     // 23.4.4.3, element access:
@@ -175,6 +177,10 @@ namespace btree {
     typedef std::pair<Key, T>      leaf_value;
     typedef std::pair<node*, Key>  branch_value;  // first is pointer to child node
 
+    static Key& key(leaf_value& v) {return v.first;}
+    static Key& key(branch_value& v) {return v.second;}
+    static T& mapped_value(leaf_value& v) {return v.second;}
+    static node*& mapped_value(branch_value& v) {return v.first;}
 
     //----------------------------------------------------------------------------------//
     //                             private nested classes                               //
@@ -213,16 +219,11 @@ namespace btree {
     public:
       uint16_t        _height;          // 0 for a leaf node
       uint16_t        _size;
-      node*           _parent_node;     // 0 for the root node
+      branch_node*    _parent_node;     // 0 for the root node
       union
       {
         branch_value* _parent_element;  // non-root node
         mbt_map*      _owner;           // root node
-      };
-      union
-      {
-        leaf_value    _leaf_values[1];     // actual size determined by constructor
-        branch_value  _branch_values[1];   // ditto
       };
 
       uint16_t      height() const                  {return _height;}
@@ -230,25 +231,53 @@ namespace btree {
       bool          is_branch() const               {return _height != 0;}
       bool          is_root() const                 {return _parent_node == 0;}
       std::size_t   size() const                    {return _size;}
-      node*         parent_node() const             {return _parent_node;}
+      branch_node*  parent_node() const             {return _parent_node;}
       branch_value* parent_element() const          {return _parent_element;}
       mbt_map*      owner() const                   {return _owner;}
 
       void          height(uint16_t h)              {_height = h;}
       void          size(std::size_t n)             {_size = n;}
-      void          parent_node(node* p)            {_parent_node = p;}
+      void          parent_node(branch_node* p)     {_parent_node = p;}
       void          parent_element(branch_value* p) {_parent_element = p;}
       void          owner(mbt_map* o)               {_owner = o;}
+    };
 
-      leaf_value*   leaf_begin()                    {return _leaf_values;}
-      leaf_value*   leaf_end()                      {return _leaf_values + _size;}
+    //------------------------------  class leaf_node  ---------------------------------//
 
-      branch_value* branch_begin()                  {return _branch_values;}
-      branch_value* branch_end()                    {return _branch_values + _size;}
-      // pseudo-element branch_end()->first is valid; b-tree branches have size() + 1
+    class leaf_node : public node
+    {
+    public:
+      typedef mbt_map::leaf_value   value_type;
+      typedef mbt_map::mapped_type  mapped_type;
+
+      leaf_value     _leaf_values[1];               // actual size determined at runtime
+
+      leaf_value*    begin()                        {return _leaf_values;}
+      leaf_value*    end()                          {return _leaf_values + node::_size;}
+
+      leaf_node*     next_node();  // returns next leaf node; root node if end
+
+      static std::size_t max_size(const mbt_map& m) {return m.m_max_leaf_size;}
+     };
+
+    //-----------------------------  class branch_node  --------------------------------//
+
+    class branch_node : public node
+    {
+    public:
+      typedef mbt_map::branch_value  value_type;
+      typedef node*                  mapped_type;
+
+      branch_value   _branch_values[1];             // actual size determined at runtime
+
+      branch_value*  begin()                        {return _branch_values;}
+      branch_value*  end()                          {return _branch_values + node::_size;}
+      // pseudo-element end()->first is valid; b-tree branches have size() + 1
       // child pointers - see your favorite computer science textbook.
 
-      node*         next_node();  // returns next node at same height; root node if end
+      branch_node*   next_node();  // returns next node at same height; root node if end
+
+      static std::size_t max_size(const mbt_map& m) {return m.m_max_branch_size;}
     };
 
     //----------------------------------------------------------------------------------//
@@ -268,7 +297,7 @@ namespace btree {
         : m_node(0), m_element(0) {}
 #     endif
 
-      iterator_type(typename mbt_map::node* np,
+      iterator_type(typename mbt_map::leaf_node* np,
                     typename mbt_map::leaf_value* ep)
         : m_node(np), m_element(ep) {}
 
@@ -283,7 +312,7 @@ namespace btree {
       friend class boost::iterator_core_access;
       friend class mbt_map;
 
-      typename mbt_map::node*        m_node;      // 0 indicates end iterator
+      typename mbt_map::leaf_node*    m_node;      // 0 indicates end iterator
 
       union  // discriminated by m_node
       {
@@ -332,20 +361,28 @@ namespace btree {
       m_size = 0;
       m_max_leaf_size = node_sz / sizeof(leaf_value);
       m_max_branch_size = node_sz / sizeof(branch_value);
-      m_root = m_new_node(0U);
+      m_root = m_new_node<leaf_node>(0U);
       m_root->owner(this);
     }
 
-    union node_values
-    {
-      leaf_value    _leaf_values[1];     // actual size determined by tree constructor
-      branch_value  _branch_values[1];   // ditto
-    };
+    template <class Node>
+    Node* m_new_node(uint16_t height_);
 
-    node* m_new_node(uint16_t height_);
     void  m_new_root();
 
     iterator m_special_lower_bound(const key_type& k) const;
+
+    template <class Node>
+    void m_insert(const key_type& k, const typename Node::mapped_type& mv,
+                  Node*& np, typename Node::value_type*& ep);
+    // Requires: np points to the node where insertion is to occur
+    //           ep points to the element where insertion is to occur
+    // Effects:  Inserts k and mv at *ep. If the insertion causes a node to be split,
+    //           and the ep falls on the newly split node, np and ep are set to point to
+    //           the new node and appropriate element
+
+    static leaf_node* leaf_cast(node* np)     {return reinterpret_cast<leaf_node*>(np);}
+    static branch_node* branch_cast(node* np) {return reinterpret_cast<branch_node*>(np);}
 
   };  // class mbt_map
 
@@ -365,23 +402,22 @@ mbt_map<Key,T,Compare,Allocator>::
 //----------------------------------  m_new_node  --------------------------------------//
 
 template <class Key, class T, class Compare, class Allocator>
-typename mbt_map<Key,T,Compare,Allocator>::node*
+template <class Node>
+Node*
 mbt_map<Key,T,Compare,Allocator>::
 m_new_node(uint16_t height_)
 {
-  std::size_t node_size = sizeof(node) - sizeof(node_values) +
-   (height_ ? m_max_branch_size * sizeof(branch_value)
-            : m_max_leaf_size * sizeof(leaf_value));
+  std::size_t node_size = sizeof(Node) - sizeof(typename Node::value_type) +
+    Node::max_size(*this);
 
-  node* np = reinterpret_cast<node*>(new char[node_size]);
-#ifdef NDEBUG
+  Node* np = reinterpret_cast<Node*>(new char[node_size]);
+#ifndef NDEBUG
+  std::memset(np, 0, node_size);
+#endif
   np->height(height_);
   np->size(0);
   np->parent_node(0);
   np->parent_element(0);
-#else
-  std::memset(np, 0, node_size);
-#endif
   return np;
 }
 
@@ -395,19 +431,20 @@ m_begin() noexcept
   if (empty())
     return end();
 
-  node* np = m_root;
+  branch_node* bp = branch_cast(m_root);
 
   // work down the tree until a leaf is reached
-  while (np->is_branch())
+  while (bp->is_branch())
   {
     // create the child->parent list
-    node* child_np = np->branch_begin()->first;
-    child_np->parent_node(np);
-    child_np->parent_element(np->branch_begin());
-    np = child_np;
+    node* child = bp->begin()->first;
+    child->parent_node(bp);
+    child->parent_element(bp->begin());
+    bp = branch_cast(child);
   }
 
-  return iterator(np, np->leaf_begin());
+  leaf_node* lp = leaf_cast(bp);
+  return iterator(lp, lp->begin());
 }
 
 //-----------------------------  m_special_lower_bound()  ------------------------------//
@@ -417,33 +454,34 @@ typename mbt_map<Key,T,Compare,Allocator>::iterator
 mbt_map<Key,T,Compare,Allocator>::
 m_special_lower_bound(const key_type& k) const
 {
-  node* np = m_root;
+  branch_node* bp = branch_cast(m_root);
 
   // search branches down the tree until a leaf is reached
-  while (np->is_branch())
+  while (bp->is_branch())
   {
     branch_value* low
-      = std::lower_bound(np->branch_begin(), np->branch_end(), k, branch_comp());
+      = std::lower_bound(bp->begin(), bp->end(), k, branch_comp());
 
     if ( /*(header().flags() & btree::flags::unique)
-      &&*/ low != np->branch_end()
+      &&*/ low != bp->end()
       && !key_comp()(k, low->second)) // if k isn't less that low key, low is equal
       ++low;                         // and so must be incremented; this follows from
                                      // the branch node invariant for unique containers
 
     // create the child->parent list
     node* child = low->first;
-    child->parent_node(np);
+    child->parent_node(bp);
     child->parent_element(low);
 
-    np = child;
+    bp = branch_cast(child);
   }
 
   //  search leaf
+  leaf_node* lp = leaf_cast(bp);
   leaf_value* low
-    = std::lower_bound(np->leaf_begin(), np->leaf_end(), k, value_comp());
+    = std::lower_bound(lp->begin(), lp->end(), k, value_comp());
 
-  return iterator(np, low);
+  return iterator(lp, low);
 }
 
 //----------------------------------- m_new_root() -------------------------------------//
@@ -455,13 +493,11 @@ m_new_root()
 {
   // create a new root containing only the end pseudo-element
   node* old_root = m_root;
-  m_root = m_new_node(old_root->height()+1);
-  m_root->branch_begin()->first = old_root;
-  m_root->size(0);  // the end pseudo-element doesn't count as an element
-  m_root->parent_node(0);
+  m_root = m_new_node<branch_node>(old_root->height()+1);
+  m_root->begin()->first = old_root;
   m_root->owner(this);
   old_root->parent_node(m_root);
-  old_root->parent_element(m_root->branch_begin());
+  old_root->parent_element(m_root->begin());
 }
 
 //-----------------------------------  insert()  ---------------------------------------//
@@ -473,33 +509,51 @@ insert(const value_type& x)
 {
   iterator insert_point = m_special_lower_bound(x.first);
 
-  std::cout << "***" << (insert_point.m_element == insert_point.m_node->leaf_end())
+  std::cout << "***" << (insert_point.m_element == insert_point.m_node->end())
   << (key_comp()(x.first, insert_point->first))
   << (key_comp()(insert_point->first, x.first)) << std::endl;
 
-  bool unique = insert_point.m_element == insert_point.m_node->leaf_end()
+  bool unique = insert_point.m_element == insert_point.m_node->end()
          || key_comp()(x.first, insert_point->first)
          || key_comp()(insert_point->first, x.first);
 
   if (!unique)
     return std::pair<iterator, bool>(insert_point, false);
 
-  //  do the insert
+  m_insert(x.first, x.second, insert_point.m_node, insert_point.m_element);
+  return std::pair<iterator, bool>(insert_point, true);
+}
 
-  node*        np = insert_point.m_node;
-  leaf_value*  insert_begin = insert_point.m_element;
-  node*        np2 = 0;
+//----------------------------------  m_insert()  --------------------------------------//
 
-  BOOST_ASSERT_MSG(np->is_leaf(), "internal error");
-  BOOST_ASSERT_MSG(np->size() <= m_max_leaf_size, "internal error");
+template <class Key, class T, class Compare, class Allocator>
+template <class Node>
+void
+mbt_map<Key,T,Compare,Allocator>::
+m_insert(const key_type& k, const typename Node::mapped_type& mv,
+              Node*& np_, typename Node::value_type*& ep_)
+// Requires: np points to the node where insertion is to occur
+//           ep points to the element where insertion is to occur
+// Effects:  Inserts k and v at *ep. If the insertion causes a node to be split,
+//           and the ep falls on the newly split node, np and ep are set to point to
+//           the new node and appropriate element
 
-  if (np->size() == m_max_leaf_size)  // if no room on node, node must be split
-  {
-    if (np->is_root()) // splitting the root?
-      m_new_root();  // create a new root
+{
 
-    np2 = m_new_node(np->height());  // create the new node
+  Node*                       np = np_;
+  typename Node::value_type*  insert_begin = ep_;
+  Node*                       np2 = 0;
 
+//  BOOST_ASSERT_MSG(np->is_leaf(), "internal error");
+//  BOOST_ASSERT_MSG(np->size() <= m_max_leaf_size, "internal error");
+//
+//  if (np->size() == m_max_leaf_size)  // if no room on node, node must be split
+//  {
+//    if (np->is_root()) // splitting the root?
+//      m_new_root();  // create a new root
+//
+//    np2 = m_new_node(np->height());  // create the new node
+//
 //    // ck pack conditions now, since leaf seq list update may chg header().last_node_id()
 //    if (m_ok_to_pack
 //        && (insert_begin != np->leaf().end() || np->node_id() != header().last_node_id()))
@@ -517,33 +571,34 @@ insert(const value_type& x)
 //      ++m_size;
 //      return const_iterator(np2, np2->leaf().begin());
 //    }
+//
+//    // split node np by moving half the elements to node np2
+//    np2->size(np->size() / 2);  // round down to minimize move size
+//    np->size(np->size() - np2->size());
+//    std::move(np->begin() + np->size(), np->end(), np2->begin());
+//
+//    // TODO: if the insert point will fall on the new node, it would be faster to
+//    // copy the portion before the insert point, copy the value being inserted, and
+//    // finally copy the portion after the insert point. However, that's a fair amount of
+//    // additional code for something that only happens on half of all leaf splits on average.
+//
+//    // adjust np and insert_begin if they now fall on the new node due to the split
+//    if (np->end() <= insert_begin)
+//    {
+//      insert_begin = np2->begin() + (insert_begin - np->end());
+//      np = np2;
+//    }
+//  }
 
-    // split node np by moving half the elements to node np2
-    np2->size(np->size() / 2);  // round down to minimize move size
-    np->size(np->size() - np2->size());
-    std::move(np->leaf_begin() + np->size(), np->leaf_end(), np2->leaf_begin());
-
-    // TODO: if the insert point will fall on the new node, it would be faster to
-    // copy the portion before the insert point, copy the value being inserted, and
-    // finally copy the portion after the insert point. However, that's a fair amount of
-    // additional code for something that only happens on half of all leaf splits on average.
-
-    // adjust np and insert_begin if they now fall on the new node due to the split
-    if (np->leaf_end() <= insert_begin)
-    {
-      insert_begin = np2->leaf_begin() + (insert_begin - np->leaf_end());
-      np = np2;
-    }
-  }
-
-  BOOST_ASSERT(insert_begin >= np->leaf_begin());
-  BOOST_ASSERT(insert_begin <= np->leaf_end());
+  BOOST_ASSERT(insert_begin >= np->begin());
+  BOOST_ASSERT(insert_begin <= np->end());
 
   // make room for insert
-  std::move_backward(insert_begin, np->leaf_end(), np->leaf_end()+1);
+  std::move_backward(insert_begin, np->end(), np->end()+1);
 
   //  insert x at insert_begin
-  *insert_begin = x;
+  key(*insert_begin) = k;
+  mapped_value(*insert_begin) = mv;
   ++np->_size;
   ++m_size;
 
@@ -556,33 +611,61 @@ insert(const value_type& x)
 //  }
 
 //std::cout << "***insert done" << std::endl;
-  return std::pair<iterator, bool>(iterator(np, insert_begin), true);
 }
 
-//------------------------------  node::next_node()  -----------------------------------//
+//------------------------------  leaf_node::next_node()  ------------------------------//
 
 template <class Key, class T, class Compare, class Allocator>
-typename mbt_map<Key,T,Compare,Allocator>::node*
-mbt_map<Key,T,Compare,Allocator>::node::
-next_node()  // return next node at current height, root_node if end
+typename mbt_map<Key,T,Compare,Allocator>::leaf_node*
+mbt_map<Key,T,Compare,Allocator>::leaf_node::
+next_node()  // return next leaf_node, root_node if end
 {
-  if (is_root())
+  if (this->is_root())
     return this;
 
-  node*          parent_np = parent_node();
-  branch_value*  parent_ep = parent_element();
+  branch_node*   parent_np = this->parent_node();
+  branch_value*  parent_ep = this->parent_element();
 
-  if (parent_ep != parent_np->branch_end())
+  if (parent_ep != parent_np->end())
     ++parent_ep;
   else
   {
-    parent_np = parent_node()->next_node();
+    parent_np = this->parent_node()->next_node();
     if (parent_np->is_root())
-      return parent_np;
-    parent_ep = parent_np->branch_begin();
+      return leaf_cast(parent_np);
+    parent_ep = parent_np->begin();
   }
 
-  node* np = parent_ep->first;
+  leaf_node* np = leaf_cast(parent_ep->first);
+  np->parent_node(parent_np);
+  np->parent_element(parent_ep);
+  return np;
+}
+
+//------------------------------  branch_node::next_node()  ----------------------------//
+
+template <class Key, class T, class Compare, class Allocator>
+typename mbt_map<Key,T,Compare,Allocator>::branch_node*
+mbt_map<Key,T,Compare,Allocator>::branch_node::
+next_node()  // return next branch_node at current height, root_node if end
+{
+  if (this->is_root())
+    return this;
+
+  branch_node*   parent_np = this->parent_node();
+  branch_value*  parent_ep = this->parent_element();
+
+  if (parent_ep != parent_np->end())
+    ++parent_ep;
+  else
+  {
+    parent_np = this->parent_node()->next_node();
+    if (parent_np->is_root())
+      return parent_np;
+    parent_ep = parent_np->begin();
+  }
+
+  branch_node* np = branch_cast(parent_ep->first);
   np->parent_node(parent_np);
   np->parent_element(parent_ep);
   return np;
@@ -599,18 +682,18 @@ increment()
   BOOST_ASSERT_MSG(m_element, "attempt to increment uninitialized iterator");
   BOOST_ASSERT_MSG(m_node, "attempt to increment end iterator");
   BOOST_ASSERT(m_node->is_leaf());
-  BOOST_ASSERT(m_element >= m_node->leaf_begin());
-  BOOST_ASSERT(m_element < m_node->leaf_end());
+  BOOST_ASSERT(m_element >= m_node->begin());
+  BOOST_ASSERT(m_element < m_node->end());
 
-  if (++m_element != m_node->leaf_end())
+  if (++m_element != m_node->end())
     return;
 
   m_node = m_node->next_node();  // next leaf node, or root node if end
 
   if (!m_node->is_root())
   {
-    m_element = m_node->leaf_begin();
-    BOOST_ASSERT(m_element != m_node->leaf_end());
+    m_element = m_node->begin();
+    BOOST_ASSERT(m_element != m_node->end());
   }
   else // end() reached
   {
